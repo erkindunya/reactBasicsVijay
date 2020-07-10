37bffbbd6b0b5e17d76fe9fb21cdb76813ec8d06
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-10 12:59:00 +0100

    Product updated with State

--- src/Product.tsx
+++ src/Product.tsx
@@ -7,18 +7,34 @@ interface IProductProps {
     item: IProduct
 }
 
-export class Product extends React.Component<IProductProps, any> {
+interface IProductState {
+    data: IProduct;
+}
+
+export class Product extends React.Component<IProductProps, IProductState> {
     constructor(props: IProductProps) {
         super(props);
 
+        this.state = {
+            data: props.item
+        };
     }
 
     public render(): JSX.Element {
         return <div className="product">
-            {this.props.item.id} <br />
-            {this.props.item.name}  <br />
-            {this.props.item.cat} <br />
-            {this.props.item.price}
+            {this.state.data.id} <br />
+            {this.state.data.name}  <br />
+            {this.state.data.cat} <br />
+            {this.state.data.price} &nbsp;
+
+            <input type="button" onClick={() => {
+                let item = this.state.data;
+                item.price += 10;
+
+                this.setState({
+                    data: item
+                });
+            }} value=" + " />
         </div>;
     }
 }
