82421581b2d5cdc9a9fc2d904ee224b80339f70a
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-10 12:14:58 +0100

    product ts is reformated to for 2 items

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,24 @@ import { Hello } from "./Hello";
 import { Product } from './Product';
 
 function App() {
+
+  let p = {
+    id: 1001,
+    name: "Nose Plier",
+    cat: "Tools",
+    price: 65
+  };
+  let p2 = {
+    id: 1002,
+    name: "cutting Plier",
+    cat: "Tools",
+    price: 300
+  };
+
   return (
     <div className="App">
-      <Product id={1001} name="Nose Plier" cat="Tools" price={65} />
+      <Product id={p.id} name={p.name} cat={p.cat} price={p.price} />
+      <Product id={p2.id} name={p2.name} cat={p2.cat} price={p2.price} />
     </div>
   );
 }
