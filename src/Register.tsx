0f706a744c9adb4ab33d593a39058bbce5bdad09
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-17 13:07:32 +0100

    before adding ref as on react you can not add ID

--- src/Register.tsx
+++ src/Register.tsx
@@ -5,9 +5,12 @@ export class Register extends React.Component<any, any> {
     super(props);
 
     this.state = {
-      name: "Enver",
-      email: "dsdds@gmail.com",
-      phone: "dsadsadsadsad"
+      name: "Vijay",
+      email: "vijay@abc.com",
+      phone: "044-78541254",
+      subscribe: true,
+      membership: "Free",
+      payment: "3"
     };
   }
 
@@ -15,22 +18,47 @@ export class Register extends React.Component<any, any> {
 
     return <div>
       <h2>Register</h2>
-                Name: <input type="text" onChange={this.nameChanged} value={(this.state.name)} /> <br />
-                Email: <input type="text" onChange={event => this.setState({ email: event.currentTarget.value })} value={(this.state.email)} /> <br />
-                Phone: <input type="text" onChange={event => this.setState({ phone: event.currentTarget.value })} value={(this.state.phone)} /> <br /><br />
+
+                Name: <input type="text" name="name" onChange={this.handleChange} value={this.state.name} /> <br />
+
+                Email: <input type="text" name="email" onChange={this.handleChange} value={this.state.email} /> <br />
+
+                Phone: <input type="text" name="phone" onChange={this.handleChange} value={this.state.phone} /> <br /><br />
+      <input type="checkbox" name="subscribe" checked={this.state.subscribe} onChange={this.handleChange} /> Subscribe to newsletter <br />
+
+      <label><h3>Choose Membership:</h3>
+        <input type="radio" name="membership" value="Free" onChange={this.handleChange} /> Free Plan <br />
+        <input type="radio" name="membership" value="Basic" onChange={this.handleChange} /> Basic Plan ($5/mo) <br />
+        <input type="radio" name="membership" value="Premium" onChange={this.handleChange} /> Premium Plan ($7/mo) <br />
+      </label><br />
+
+                Payment By:
+                <select name="payment" value={this.state.payment} onChange={this.handleChange}>
+        <option value="1">Credit Card</option>
+        <option value="2">Debit Card</option>
+        <option value="3">Pay Pal</option>
+      </select> <br /><br />
+
       <input type="button" value=" Register " /><br /><br />
+
       <div>
         Registration Information  <br />
                     Name: {this.state.name}<br />
                     Email: {this.state.email}<br />
                     Phone {this.state.phone}<br />
+                    Subscribe: {this.state.subscribe ? "Yes" : "No"} <br />
+                    Membership : {this.state.membership} <br />
+                    Payment : {this.state.payment}
       </div>
     </div>;
   }
 
-  private nameChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+  private handleChange = (event: any) => {
+    let name = event.target.name;
+
     this.setState({
-      name: event.currentTarget.value
+      [name]: event.target.name === "subscribe" ? event.target.checked : event.target.value
     });
   }
+
 }
