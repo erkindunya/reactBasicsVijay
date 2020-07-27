8374891fd8b329c4e00bb1887144a2e7fd92474a
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-27 11:57:53 +0100

    with book in the state

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Counter } from "./Counter";
-import { countReset } from 'console';
-
 
 function App() {
   return (
     <div className="App">
-      <Counter initial={ 10 } />
+      <Counter initial={ 10 } book={{
+        id: 1,
+        name: "Sample book"
+      }} />
     </div>
   );
 }
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,17 +2,34 @@ import * as React from "react";
 
 export interface ICounterProps {
     initial: number;
+    book: IBook;
+}
+
+interface IBook {
+    id: number;
+    name: string;
 }
 
 // <Counter initial={ 1 } />
 export function Counter(props: ICounterProps) : JSX.Element {
     let [count,setCount] = React.useState(props.initial);
+    let [book,setBook] = React.useState(props.book);
 
     return <div>
         Count: { count } <br/>
+        Book Info : <br/>
+        { book.id } : { book.name }<br/>
 
-        <input type="button" value=" + " onClick={ () => {
+        <input type="button" value="Change" onClick={ () => {
             setCount(count+1);
+
+            let newName = "Sample book: " + new Date().getTime();
+
+            setBook({
+                name: newName,
+                id: book.id
+            });
+
             console.log("Count=" + count);
         }} />
     </div>;
