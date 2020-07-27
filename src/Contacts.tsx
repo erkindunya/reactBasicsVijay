f2a1d1d3b1837173119e7e9edce88dda897e6c05
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-27 12:30:04 +0100

    useEffect example -

--- src/Contacts.tsx
+++ src/Contacts.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 interface IContact {
     name: string;
@@ -21,8 +21,22 @@ const data : IContact[] = [
 ];
 
 export function Contacts() : JSX.Element {
+    let flag : number = 0;
+
     let [contacts,setContacts] = useState(data);
 
+    useEffect(()=> {
+        // LIKE componentDidMount - Fires only once after render
+        console.log("ComponentDidMount->Fired after render!");
+        flag = 1;
+    },[flag]);
+
+    useEffect(()=> {
+        // LIKE componentDidUpdate - Fires after ever render
+        console.log("CompoentDidUpdate->Fired after render!");
+    });
+
+    console.log("Render firing...");
     return <div>
         {
             contacts.map((c: IContact) => <div>
